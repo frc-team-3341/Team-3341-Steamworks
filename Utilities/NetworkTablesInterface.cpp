1edf7743609480291af5a06696bee70468bf4ecc
dhrutip <dhruti.pandya@gmail.com>
2017-01-17 19:33:48 -0800

    Network Tables Interface

---
+++ Utilities/NetworkTablesInterface.cpp
@@ -0,0 +1,59 @@
+/*
+ * NetworkTablesInterface.cpp
+ *
+ *  Created on: Jan 17, 2017
+ *      Author: Dhruti
+ */
+
+#include "NetworkTablesInterface.h"
+
+using namespace frc;
+
+/*NetworkTablesInterface::NetworkTablesInterface() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NetworkTablesInterface::~NetworkTablesInterface() {
+	// TODO Auto-generated destructor stub
+}*/
+
+bool NetworkTablesInterface::gearFound()
+{
+	return NetworkTable::GetTable("cv")->GetNumber("gearFound", false);
+	//returns true if the gear has been detected
+}
+
+double NetworkTablesInterface::getGearDistance()
+{
+	return NetworkTable::GetTable("cv")->GetNumber("gearDistance", -1.0);
+}
+
+double NetworkTablesInterface::getGearAltitude()
+{
+	return NetworkTable::GetTable("cv")->GetNumber("gearAltitude", -1.0);
+}
+
+double NetworkTablesInterface::getGearAzimuth()
+{
+	return NetworkTable::GetTable("cv")->GetNumber("gearAzimuth", -1.0);
+}
+bool NetworkTablesInterface::boilerFound()
+{
+	return NetworkTable::GetTable("cv")->GetBoolean("boilerFound", false);
+}
+
+double NetworkTablesInterface::getBoilerDistance()
+{
+	return NetworkTable::GetTable("cv")->GetNumber("boilerDistance", -1.0);
+}
+
+double NetworkTablesInterface::getBoilerAltitude()
+{
+	return NetworkTable::GetTable("cv")->GetNumber("boilerAltitude", -1.0);
+}
+
+double NetworkTablesInterface::getBoilerAzimuth()
+{
+	return NetworkTable::GetTable("cv")->GetNumber("boilerAzimuth", -1.0);
+}
---
+++ Utilities/NetworkTablesInterface.h
@@ -0,0 +1,25 @@
+/*
+ * NetworkTablesInterface.h
+ *
+ *  Created on: Jan 17, 2017
+ *      Author: Dhruti
+ */
+
+#ifndef UTILITIES_NETWORKTABLESINTERFACE_H_
+#define UTILITIES_NETWORKTABLESINTERFACE_H_
+
+class NetworkTablesInterface {
+public:
+	//NetworkTablesInterface(); NOT REQUIRED
+	//virtual ~NetworkTablesInterface(); NOT REQUIRED
+	static bool gearFound(); //checks if the gear has been detected
+	static double getGearDistance(); //gives the distance to the gear
+	static double getGearAltitude(); //gives the altitude for the gear
+	static double getGearAzimuth(); //gives the azimuth for the gear
+	static bool boilerFound(); //checks if the boiler has been detected
+	static double getBoilerDistance(); //gives the distance to the boiler
+	static double getBoilerAltitude(); //gives the altitude to the boiler
+	static double getBoilerAzimuth(); //gives the azimuth to the boiler
+};
+
+#endif /* UTILITIES_NETWORKTABLESINTERFACE_H_ */
